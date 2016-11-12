58c3992b39ccba5fcdbf45864cb6d0121fa0ae56
Kion <admin@airbears2-10-142-148-193.airbears2.1918.berkeley.edu>
2016-11-12 12:50:02 -0800

    Adding structures

--- contract/chainge.sol
+++ contract/chainge.sol
@@ -4,21 +4,50 @@ contract token {function transfer(address receiver, uint amount) { }}
 contract Chainge {
 
   struct Share {
-
-    
+    uint public costInEther,
+    address winner;
   }
 
+  struct Ballot {
+    bytes32 name,
+    bytes32 desc,
+    bool satisfied,
+    Share[] shares,
+    uint jackpot;
+  }
+
+  mapping(address => uint256) public userSpace;
   uint principle;
-  uint jackpot;
   address initialInvestor;
   uint public deadline;
+  Ballot ballot;
 
-  function Chainge(address _initialInvestor, uint _principle, uint lengthInDays) {
+  function Chainge(address _initialInvestor, uint _principle, uint _lengthInDays, bytes32 _name, bytes32 _desc) {
     principle = _principle;
-    jackpot = _principle;
+    ballot.jackpot = _principle;
+    ballot.name = _name;
+    ballot.desc = _desc;
     initialInvestor = _initialInvestor;
-    deadline = now + lengthInDays * 1 days;
+    deadline = now + _lengthInDays * 1 days;
   }
 
+  modifier ballotSatisfied() { if(!ballot.satisfied) _; }
+  modifier deadlinePassed() { if(now >= deadline) _; }
+
+  function returnPrinciple() deadlinePassed {
+
+
+  }
+
+  function payOut() ballotSatisfied {
+    for(int i = 0; i < ballot.shares.length; i++) {
+
+    }
+    userSpace[winner]
+  }
+
+  function() {
+    throw;
+  }
 
 }
