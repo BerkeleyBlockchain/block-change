6e607c5325435706ad794a2719c70846cc6c320c
nihar97 <ndalal@berkeley.edu>
2016-11-12 17:25:52 -0800

    Wrote several functions

--- contract/chainge.sol
+++ contract/chainge.sol
@@ -13,12 +13,14 @@ contract Chainge {
   }
 }
 
+//question to Kion and Max. Is it necessary that we must have only one initial investor? Couldn't there be several initial investors who
+// willing to make donations?
 contract Ballot {
 
   struct Share {
     uint public costInEther,
     address owner,
-    uint expirationDate,
+    uint expirationDate, //how do we determine this? - Nihar
     bool assigned;
   }
 
@@ -47,7 +49,8 @@ contract Ballot {
     deadline = _deadline;
     jackpot = 0;
     satisfied = false;
-    shares = new Share[principle];
+    shares = new Share[principle]; //why should there be a number of shares equivalent to the principle? - Nihar
+    //again we need to do something here with expiration date
   }
 
   function findAvailableShare() returns (Share) {
@@ -61,8 +64,47 @@ contract Ballot {
     }
   }
 
+  function intitalInvestement(address _initinv){
+    require(msg.value == principle);
+    initialInvestor = _initinv;
+
+  }
   function returnPrinciple() principleMissed {
       initialInvestor.send(principle);
+  }
+
+  function buyShare(address _owner){
+    Share share = findAvailableShare();
+    require(msg.value == share.costInEther);
+    share.assigned = true;
+    share.owner = _owner;
+    jackpot = jackpot + share.costInEther;
+    //do something here with expiration date or maybe not
+  }
+
+  function determineCost(){
+    //not sure how we intend on determining the current cost of the share
+  }
+  //helper function to find all available shares
+  function findAllAvailableShares() returns (Share[]){
+    Share[] avaShares;
+    uint count = 0;
+    for(int i = 0; i < shares.length; i++) {
+      if(shares[i].assigned && shares[i].expirationDate >= now) {
+        shares[i].assigned = false;
+      }
+      if(!shares[i].assigned) {
+        avaShares[count] = shares[i];
+        count = count + 1;
+      }
+    }
+    return avaShares;
+  }
+  function changeCostOfAllAvailable(uint newCost){
+    Share[] avaShares = findAllAvailableShares();
+    for(int i =0; i < avaShares.length; i++){
+      avaShares[i].costInEther = newCost;
+    }
   }
 
   function payOut() ballotSatisfied {
