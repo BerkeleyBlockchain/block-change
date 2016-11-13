02bc1c34a709f31117c420fd9ccbd06166a246fc
Max Fang <maxfangx@gmail.com>
2016-11-13 08:03:15 -0800

    new deploy made

--- contracts/blockchange.sol
+++ contracts/blockchange.sol
@@ -1,5 +1,25 @@
 pragma solidity ^0.4.4;
 
+contract contractGenerator {
+
+  uint contractCount = 0;
+  mapping(uint => BlockChange) public blockchangeIndex;
+
+  function contractGenerator(){}
+
+  function createNewBlockchange(
+        bytes32 _name,
+        bytes32 _desc,
+        uint _ratioOfTotalShareValueToJackpot,
+        uint _cycleLength,
+        uint _initWait
+    ) {
+      blockchangeIndex[contractCount++] = new BlockChange(_name, _desc,
+        _ratioOfTotalShareValueToJackpot, _cycleLength, _initWait);
+  }
+
+}
+
 contract BlockChange {
 
   address public owner;
